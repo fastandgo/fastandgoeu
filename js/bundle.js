02ade2c6097fa62f9abe3b954386a6227de120c9
Thomas Bekaert <thomas@bekaert.me>
2019-01-11 15:02:51 +0100

    Add contact form

--- js/bundle.js
+++ js/bundle.js
@@ -8,7 +8,9 @@ var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 
 
 
 
-
+function unwrapExports (x) {
+	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+}
 
 function createCommonjsModule(fn, module) {
 	return module = { exports: {} }, fn(module, module.exports), module.exports;
@@ -8350,9 +8352,235 @@ var alertbox = new Alertbox();
   closingButtons: Array.from(document.querySelectorAll('.js-close-bullets'))
 });
 
-"use strict";
+var EmailJSResponseStatus_1 = createCommonjsModule(function (module, exports) {
+    "use strict";
 
-// import "./molecules/contact.js";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var EmailJSResponseStatus = /** @class */function () {
+        function EmailJSResponseStatus(httpResponse) {
+            this.status = httpResponse.status;
+            this.text = httpResponse.responseText;
+        }
+        return EmailJSResponseStatus;
+    }();
+    exports.EmailJSResponseStatus = EmailJSResponseStatus;
+});
+
+unwrapExports(EmailJSResponseStatus_1);
+
+var UI_1 = createCommonjsModule(function (module, exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var UI = /** @class */function () {
+        function UI() {}
+        UI.clearAll = function (form) {
+            form.classList.remove(this.PROGRESS);
+            form.classList.remove(this.DONE);
+            form.classList.remove(this.ERROR);
+        };
+        UI.progressState = function (form) {
+            this.clearAll(form);
+            form.classList.add(this.PROGRESS);
+        };
+        UI.successState = function (form) {
+            form.classList.remove(this.PROGRESS);
+            form.classList.add(this.DONE);
+        };
+        UI.errorState = function (form) {
+            form.classList.remove(this.PROGRESS);
+            form.classList.add(this.ERROR);
+        };
+        UI.PROGRESS = 'emailjs-sending';
+        UI.DONE = 'emailjs-success';
+        UI.ERROR = 'emailjs-error';
+        return UI;
+    }();
+    exports.UI = UI;
+});
+
+unwrapExports(UI_1);
+
+var source$1 = createCommonjsModule(function (module, exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", { value: true });
+
+    var _userID = null;
+    var _origin = 'https://api.emailjs.com';
+    function sendPost(url, data, headers) {
+        if (headers === void 0) {
+            headers = {};
+        }
+        return new Promise(function (resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener('load', function (event) {
+                var responseStatus = new EmailJSResponseStatus_1.EmailJSResponseStatus(event.target);
+                if (responseStatus.status === 200 || responseStatus.text === 'OK') {
+                    resolve(responseStatus);
+                } else {
+                    reject(responseStatus);
+                }
+            });
+            xhr.addEventListener('error', function (event) {
+                reject(new EmailJSResponseStatus_1.EmailJSResponseStatus(event.target));
+            });
+            xhr.open('POST', url, true);
+            for (var key in headers) {
+                xhr.setRequestHeader(key, headers[key]);
+            }
+            xhr.send(data);
+        });
+    }
+    function appendGoogleCaptcha(templatePrams) {
+        var element = document.getElementById('g-recaptcha-response');
+        if (element && element.value) {
+            templatePrams['g-recaptcha-response'] = element.value;
+        }
+        element = null;
+        return templatePrams;
+    }
+    /**
+     * Initiation
+     * @param {string} userID - set the EmailJS user ID
+     * @param {string} origin - set the EmailJS origin
+     */
+    function init(userID, origin) {
+        _userID = userID;
+        _origin = origin || 'https://api.emailjs.com';
+    }
+    exports.init = init;
+    /**
+     * Send a template to the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {Object} templatePrams - the template params, what will be set to the EmailJS template
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+    function send(serviceID, templateID, templatePrams, userID) {
+        var params = {
+            lib_version: '2.3.2',
+            user_id: userID || _userID,
+            service_id: serviceID,
+            template_id: templateID,
+            template_params: appendGoogleCaptcha(templatePrams)
+        };
+        return sendPost(_origin + '/api/v1.0/email/send', JSON.stringify(params), {
+            'Content-type': 'application/json'
+        });
+    }
+    exports.send = send;
+    /**
+     * Send a form the specific EmailJS service
+     * @param {string} serviceID - the EmailJS service ID
+     * @param {string} templateID - the EmailJS template ID
+     * @param {string | HTMLFormElement} form - the form element or selector
+     * @param {string} userID - the EmailJS user ID
+     * @returns {Promise<EmailJSResponseStatus>}
+     */
+    function sendForm(serviceID, templateID, form, userID) {
+        if (typeof form === 'string') {
+            form = document.querySelector(form);
+        }
+        if (!form || form.nodeName !== 'FORM') {
+            throw 'Expected the HTML form element or the style selector of form';
+        }
+        UI_1.UI.progressState(form);
+        var formData = new FormData(form);
+        formData.append('lib_version', '2.3.2');
+        formData.append('service_id', serviceID);
+        formData.append('template_id', templateID);
+        formData.append('user_id', userID || _userID);
+        return sendPost(_origin + '/api/v1.0/email/send-form', formData).then(function (response) {
+            UI_1.UI.successState(form);
+            return response;
+        }, function (error) {
+            UI_1.UI.errorState(form);
+            return Promise.reject(error);
+        });
+    }
+    exports.sendForm = sendForm;
+});
+
+unwrapExports(source$1);
+var source_1 = source$1.init;
+var source_2 = source$1.send;
+
+/**
+ * Init desc module.
+ *
+ * @module desc
+ * @example
+ * // import it in your Javascript file
+ * import "path/to/desc.js"
+ * */
+
+(function (_ref) {
+  var $form = _ref.$form;
+
+  source_1('user_vflttScXmk82WIBRJ6dEK');
+  var values = {};
+  var validator = {
+    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    phone: /^((\+[\d]{2}|0)[\s\d]{0,2})([\s-]?[\d]{2}){4,5}$/
+  };
+  var inputs = Array.from($form.elements).filter(function (e) {
+    return e.tagName !== 'BUTTON';
+  });
+  var button = Array.from($form.elements).filter(function (e) {
+    return e.tagName === 'BUTTON';
+  })[0];
+  var validate = function validate() {
+    var isFormValid = Object.keys(values).filter(function (k) {
+      if (k === 'message') return false;
+      if (validator[k] && values[k].match(validator[k]) === null) return true;
+      return !values[k];
+    }).length === 0;
+    if (isFormValid && button.classList.contains('disabled')) {
+      button.classList.remove('disabled');
+    }
+    if (!isFormValid && !button.classList.contains('disabled')) {
+      button.classList.add('disabled');
+    }
+  };
+  var saveData = function saveData(input) {
+    values[input.name] = input.type === 'checkbox' ? input.checked : input.value;
+    validate();
+  };
+  var resetForm = function resetForm() {
+    button.classList.add('disabled');
+    inputs.map(function (input) {
+      input.value = '';
+      input.checked = false;
+    });
+  };
+  inputs.map(function (input) {
+    var eventName = input.type === 'checkbox' ? 'click' : input.tagName === 'SELECT' ? 'change' : 'keyup';
+    saveData(input);
+    input.addEventListener(eventName, function (e) {
+      saveData(input);
+    });
+  });
+
+  $form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    source_2('default_service', 'template_oO7LexQB', values).then(function (response) {
+      var $alert = alertbox.create("Votre message a bien été envoyé. Nous reviendrons rapidement vers vous.", "success", "black");
+      $form.appendChild($alert);
+      resetForm();
+    }, function (error) {
+      console.log('fail', error);
+      var $alert = alertbox.create("Une erreur est survenue lors de l'envoi de votre message. Veuillez réessayer ulteérieurement.", "alert", "white");
+      $form.appendChild($alert);
+    });
+    console.log('submit');
+  });
+})({
+  $form: document.querySelector('#contact-form')
+});
+
+"use strict";
 
 ready(function () {
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
